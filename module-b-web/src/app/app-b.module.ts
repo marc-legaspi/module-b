d1f3ed87b5f665b1bf2062d249df73a85bf4d458
Marc Erickson Legaspi <cbiz_marclegaspi@flyscoot.com>
2024-07-05 14:33:51 +0800

    fix error

--- module-b-web/src/app/app-b.module.ts
+++ module-b-web/src/app/app-b.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BCardComponent } from './b-card/b-card.component';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [AppComponent, BCardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [AppComponent, BCardComponent]
 })
--- module-b-web/src/app/b-card/b-card.component.ts
+++ module-b-web/src/app/b-card/b-card.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-b-card',
-  standalone: true,
-  imports: [],
   templateUrl: './b-card.component.html',
   styleUrl: './b-card.component.scss'
 })
